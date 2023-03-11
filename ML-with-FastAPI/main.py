ee39caa7e7ee8c802f8569d5f58e965274aa3d60
hazrulakmal <hazrulakmal121@gmail.com>
2023-03-11 01:12:16 +0000

    finish off

---
+++ ML-with-FastAPI/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from ml import obtain_image
+app = FastAPI()
+
+@app.get("/") #for this path use this function
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id, "message": "Hello World"}
+
+class Item(BaseModel):
+    name: str
+    price: float
+    tags: list[str] = []
+
+@app.post("/items/")
+def create_item(item: Item):
+    return item
+
+@app.get("/generate")
+def generate_image(
+    prompt: str,
+    *,
+    seed: int = None,
+    num_inference_steps: int = 50,
+    guidance_scale: float = 7.5
+):
+    image = obtain_image(
+        prompt,
+        num_inference_steps=num_inference_steps,
+        seed=seed,
+        guidance_scale=guidance_scale,
+    )
+    image.save("image.png")
+    return FileResponse("image.png")
--- ML-with-FastAPI/ml.py
+++ ML-with-FastAPI/ml.py
@@ -4,3 +4,31 @@ import torch
 from diffusers import StableDiffusionPipeline
 from PIL.Image import Image
 
+token_path = Path("token_key.txt")
+token = token_path.read_text().strip()
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    "CompVis/stable-diffusion-v1-4",
+    revision="fp16",
+    torch_dtype=torch.float16,
+    use_auth_token=token,
+)
+
+
+def obtain_image(
+    prompt: str,
+    *,
+    seed: int = None,
+    num_inference_steps: int = 50,
+    guidance_scale: float = 7.5,
+) -> Image:
+    generator = None if seed is None else torch.Generator("cuda").manual_seed(seed)
+    print(f"Using device: {pipe.device}")
+    image: Image = pipe(
+        prompt,
+        guidance_scale=guidance_scale,
+        num_inference_steps=num_inference_steps,
+        generator=generator,
+    ).images[0]
+    return image
+
